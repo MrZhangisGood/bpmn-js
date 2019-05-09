74d413177e0a55a8d56a7b5c9ea3f3f34a8d6a3b
Gustavo E. Jimenez Folta <gustavo.jimenez@camunda.com>
2019-05-09 17:41:21 +0200

    feat(modeling): move expanded subprocess after it replaces a shape
    This is to prevent overlapping content that is present before the
    shape that is to be replaced.

---
+++ test/spec/features/modeling/behavior/SubProcessBehaviorSpec.js
@@ -0,0 +1,54 @@
+import {
+  bootstrapModeler,
+  inject
+} from 'test/TestHelper';
+
+import coreModule from 'lib/core';
+import modelingModule from 'lib/features/modeling';
+import replaceModule from 'lib/features/replace';
+
+describe('features/modeling/behavior - subprocess', function() {
+  var diagramXML = require('./SubProcessBehavior.bpmn');
+
+  beforeEach(bootstrapModeler(
+    diagramXML, {
+      modules: [
+        coreModule,
+        modelingModule,
+        replaceModule
+      ]
+    }
+  ));
+
+
+  describe('should prevent overlap with previous content', function() {
+
+    it('when replacing a task with an expanded subprocess', inject(
+      function(elementRegistry, bpmnReplace) {
+
+        // given
+        var task = elementRegistry.get('Task'),
+            expandedSubProcess;
+
+        // when
+        expandedSubProcess = bpmnReplace.replaceElement(task, {
+          type: 'bpmn:SubProcess',
+          isExpanded: true
+        });
+
+        // y has not changed after replace and width/height are the defaults
+        var expectedBounds = {
+          x: task.x,
+          y: 21,
+          width: 350,
+          height: 200
+        };
+
+        // then
+        expect(expandedSubProcess).to.have.bounds(expectedBounds);
+      }
+    ));
+
+  });
+
+});
